ebfff0e00e29d3fc1f9b88c1b09f188a478b2383
devlouis <louisonyedi@gmail.com>
2023-10-28 13:13:29 +0100

    meme to Meme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navba from './components/Navba'
-import Meme from './components/meme'
+import Meme from './components/Meme'
 import "./components/style.css"
 
 const App = () => {
